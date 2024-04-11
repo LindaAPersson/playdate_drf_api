87a843c3ec3349c472361ed0999b136c952daff0
Linda Persson <lindapersson-@live.se>
2024-04-11 10:01:29 +0000

    <feat>[playdate]add is_organizer field and read only function

--- playdate/serializers.py
+++ playdate/serializers.py
@@ -3,9 +3,14 @@ from .models import Playdate
 
 class PlaydateSerializer(serializers.ModelSerializer):
     organizer = serializers.ReadOnlyField(source='organizer.username')
+    is_organizer = serializers.SerializerMethodField()
+
+    def get_is_organizer(self, obj):
+        request = self.context['request']
+        return request.user == obj.organizer
 
     class Meta:
         model = Playdate
         fields = [
-            'id', 'title', 'image', 'date', 'location', 'description', 'organizer', 'prize', 'created_at', 'parent_stay_required'
+            'id', 'title', 'image', 'date', 'location', 'description', 'organizer', 'prize', 'created_at', 'parent_stay_required', 'is_organizer'
         ]
--- playdate/views.py
+++ playdate/views.py
@@ -4,33 +4,38 @@ from rest_framework.views import APIView
 from rest_framework.response import Response
 from .models import Playdate
 from .serializers import PlaydateSerializer
+from drf_api.permissions import IsOwnerOrReadOnly
 
 # Create your views here.
 
 class PlaydateList(APIView):
     def get(self,request):
         playdates = Playdate.objects.all()
-        serializer = PlaydateSerializer(playdates, many=True)
+        serializer = PlaydateSerializer(playdates, many=True, context={'request': request})
         return Response(serializer.data)
 
 class PlaydateListDetail(APIView):
     serializer_class = PlaydateSerializer
-    
+    permission_classes = [IsOwnerOrReadOnly]
+
     def get_object(self, pk):
         try:
             playdate = Playdate.objects.get(pk=pk)
+            self.check_object_permissions(self.request, playdate)
             return playdate
         except Playdate.DoesNotExist:
             raise Http404
     
     def get(self,request, pk):
         playdate = self.get_object(pk)
-        serializer = PlaydateSerializer(playdate)
+        serializer = PlaydateSerializer(
+            playdate, context={'request': request}
+            )
         return Response(serializer.data)
     
     def put(self, request, pk):
         playdate = self.get_object(pk)
-        serializer = PlaydateSerializer(playdate, data=request.data)
+        serializer = PlaydateSerializer(playdate, data=request.data, context={'request': request})
         if serializer.is_valid():
             serializer.save()
             return Response(serializer.data)
