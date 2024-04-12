2bdf02481007aa1c7891d5388c20176b632abf63
Linda Persson <lindapersson-@live.se>
2024-04-12 05:36:17 +0000

    <fix>[login, logout]uppdate settings to be able to login in deployed site

---
+++ drf_api/serializers.py
@@ -0,0 +1,11 @@
+from dj_rest_auth.serializers import UserDetailsSerializer
+from rest_framework import serializers
+
+
+class CurrentUserSerializer(UserDetailsSerializer):
+    user_id = serializers.ReadOnlyField(source='user.id')
+
+    class Meta(UserDetailsSerializer.Meta):
+        fields = UserDetailsSerializer.Meta.fields + (
+            'user_id', 
+        )
