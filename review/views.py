93f9518788140e4041f18512e2c95a4b09557779
Linda Persson <lindapersson-@live.se>
2024-04-23 08:04:48 +0000

    <feat>[review]add view to review

--- review/views.py
+++ review/views.py
@@ -1,3 +1,31 @@
-from django.shortcuts import render
+from rest_framework import generics, permissions
+from django_filters.rest_framework import DjangoFilterBackend
+from drf_api.permissions import IsOwnerOrReadOnly
+from .models import Review
+from .serializers import ReviewSerializer, ReviewDetailSerializer
 
 # Create your views here.
+
+class ReviewList(generics.ListCreateAPIView):
+    
+    serializer_class = ReviewSerializer
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+    queryset = Review.objects.all()
+
+    filter_backends = [
+        DjangoFilterBackend,
+    ]
+    filterset_fields = [
+        'playdate_post', 
+        'user__username'   
+    ]
+    
+    def perform_create(self, serializer):
+        serializer.save(user=self.request.user)
+
+
+class CommentDetail(generics.RetrieveUpdateDestroyAPIView):
+    
+    permission_classes = [IsOwnerOrReadOnly]
+    serializer_class = ReviewDetailSerializer
+    queryset = Review.objects.all()
