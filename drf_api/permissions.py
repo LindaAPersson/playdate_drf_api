200a10f0cd1b40585ff771eff8d3b17ac6206f43
Linda Persson <lindapersson-@live.se>
2024-04-27 19:31:07 +0000

    <chore>[drf api]add docstrings to files

--- drf_api/permissions.py
+++ drf_api/permissions.py
@@ -2,6 +2,14 @@ from rest_framework import permissions
 
 
 class IsOwnerOrReadOnly(permissions.BasePermission):
+    """
+    Permission class to check if the requesting user is the owner of the object.
+
+    Attributes:
+        request (Request): The request being made.
+        view (View): The view associated with the request.
+        obj (object): The object being accessed.
+    """
     def has_object_permission(self, request, view, obj):
         if request.method in permissions.SAFE_METHODS:
             return True
--- drf_api/serializers.py
+++ drf_api/serializers.py
@@ -3,6 +3,12 @@ from rest_framework import serializers
 
 
 class CurrentUserSerializer(UserDetailsSerializer):
+    """
+    Serializer class to represent the current user.
+
+    Attributes:
+        user_id (ReadOnlyField): The read-only field representing the user's ID.
+    """
     user_id = serializers.ReadOnlyField(source='user.id')
 
     class Meta(UserDetailsSerializer.Meta):
